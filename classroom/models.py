cb3692993cc3a07e0833fa2067f2fd36edbf1f0b
savannah8 <willengitonga.wg@gmail.com>
2019-07-25 11:45:01 +0300

    added the quiz model

--- classroom/models.py
+++ classroom/models.py
@@ -6,3 +6,23 @@ class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
 
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    color = models.CharField(max_length=7, default='#007bff')
+
+    def __str__(self):
+        return self.name
+
+    def get_html_badge(self):
+        name = escape(self.name)
+        color = escape(self.color)
+        html = '<span class="badge badge-primary" style="background-color: %s">%s</span>' % (color, name)
+        return mark_safe(html)
+
+class Quiz(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='quizzes')
+    name = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='quizzes')
+
+    def __str__(self):
+        return self.name
