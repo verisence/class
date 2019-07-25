7c41542ee39cbc3b7dee35cc34511060bb5a64f4
Micah <micahkimathi@gmail.com>
2019-07-25 12:08:02 +0300

    Create the student sign-up form in the forms.py file

--- classroom/forms.py
+++ classroom/forms.py
@@ -16,3 +16,22 @@ class TeacherSignUpForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class StudentSignUpForm(UserCreationForm):
+    interests = forms.ModelMultipleChoiceField(
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+        student = Student.objects.create(user=user)
+        student.interests.add(*self.cleaned_data.get('interests'))
+        return user
