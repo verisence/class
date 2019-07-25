5e27668f1b552731a8639065e265d981f9891d6a
Micah <micahkimathi@gmail.com>
2019-07-25 12:06:54 +0300

    Create the teacher sign-up form in the forms.py file

--- classroom/forms.py
+++ classroom/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-from classroom.models import (Stude)
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+from django.forms.utils import ValidationError
 
-class StudeForm(forms.ModelForm):
-    class Meta:
-        model = Stude
-        exclude = ['user']
+from classroom.models import (Answer, Question, Student, StudentAnswer,
+                              Subject, User,Stude)
+
+class TeacherSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
