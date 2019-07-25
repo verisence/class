174a2723cb974f4fe0aefd2d6fa53fecd7b6f11f
Santa Claus <santa@northpole.com>
2019-07-25 12:12:50 +0300

    created classTakenQuizListView and added templates

--- classroom/views/students.py
+++ classroom/views/students.py
@@ -43,3 +43,33 @@ class StudentInterestsView(UpdateView):
         return super().form_valid(form)
 
 
+@method_decorator([login_required, student_required], name='dispatch')
+class QuizListView(ListView):
+    model = Quiz
+    ordering = ('name', )
+    context_object_name = 'quizzes'
+    template_name = 'students/quiz_list.html'
+
+    def get_queryset(self):
+        student = self.request.user.student
+        student_interests = student.interests.values_list('pk', flat=True)
+        taken_quizzes = student.quizzes.values_list('pk', flat=True)
+        queryset = Quiz.objects.filter(subject__in=student_interests) \
+            .exclude(pk__in=taken_quizzes) \
+            .annotate(questions_count=Count('questions')) \
+            .filter(questions_count__gt=0)
+        return queryset
+
+
+@method_decorator([login_required, student_required], name='dispatch')
+class TakenQuizListView(ListView):
+    model = TakenQuiz
+    context_object_name = 'taken_quizzes'
+    template_name = 'students/taken_quiz_list.html'
+
+    def get_queryset(self):
+        queryset = self.request.user.student.taken_quizzes \
+            .select_related('quiz', 'quiz__subject') \
+            .order_by('quiz__name')
+        return queryset
+
