de79c646059cb56fb3db079d0c61ede543d7d8ed
Santa Claus <santa@northpole.com>
2019-07-25 11:56:16 +0300

    defined home and rendered  home.html on classroom

--- classroom/views/classroom.py
+++ classroom/views/classroom.py
@@ -4,3 +4,12 @@ from django.views.generic import TemplateView
 
 class SignUpView(TemplateView):
     template_name = 'registration/signup.html'
+
+
+def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_teacher:
+            return redirect('teachers:quiz_change_list')
+        else:
+            return redirect('students:quiz_list')
+    return render(request, 'home.html')
